1c0091e7fa76dc786e3645baaf69adc374d03d73
zyy <zhangyiyang888@sina.cn>
2019-04-11 11:29:42 +0800

    add source

--- 5P/5P/Pandora/apps/__UNI__3C18621/www/pages/test/test.js
+++ 5P/5P/Pandora/apps/__UNI__3C18621/www/pages/test/test.js
@@ -1,13 +1,2 @@
-
-      !(function(){
-        var uniAppViewReadyCallback = function(){
-          setCssToHead(["wx-uni-image { height: ",[0,40],"; width: ",[0,40],"; }\n.",[1],"xxxxx { font-size: ",[0,72],"; }\n",],undefined,{path:"./pages/test/test.wxss"})();
+setCssToHead([".",[1],"xxxxx { font-size: ",[0,72],"; }\n.",[1],"xxxxx { height: ",[0,22],"; width: ",[0,22],"; }\n",],undefined,{path:"./pages/test/test.wxss"})();
 document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/test/test.wxml') } }));
-        }
-        if(window.__uniAppViewReady__){
-          uniAppViewReadyCallback()
-        }else{
-          document.addEventListener('uniAppViewReady',uniAppViewReadyCallback)
-        }
-      })();
-      
