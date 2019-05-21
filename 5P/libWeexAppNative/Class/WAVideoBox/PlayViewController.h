b929b68ac9e34295f74c6df472ff1dd2eec45523
zyy <zhangyiyang888@sina.cn>
2019-05-21 15:21:17 +0800

    add play controller

---
+++ 5P/libWeexAppNative/Class/WAVideoBox/PlayViewController.h
@@ -0,0 +1,19 @@
+//
+//  PlayViewController.h
+//  WAVideoBox
+//
+//  Created by 黄锐灏 on 2019/1/6.
+//  Copyright © 2019 黄锐灏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlayViewController : UIViewController
+
+- (void)loadWithUrl:(NSString *)Url ;
+
+@end
+
+NS_ASSUME_NONNULL_END
